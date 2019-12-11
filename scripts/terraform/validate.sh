b0728743810c28da091e4b12164487fb9a467454
Atlas Kerr <atlaskerr@gmail.com>
2019-12-10 22:13:26 -0600

    make validate script more verbose
    Signed-off-by: Atlas Kerr <akerr@onica.com>

--- scripts/terraform/validate.sh
+++ scripts/terraform/validate.sh
@@ -6,10 +6,12 @@ set -o pipefail
 ROOT_DIR=$PWD
 FILENAME='main.tf.json'
 for i in $(find -name $FILENAME); do
-	cd $(dirname $i)
+	DIR=$(dirname $i)
+	cd $DIR
 	if [[ ! -d ".terraform" ]]; then
 		terraform init
 	fi
+	echo $DIR
 	terraform validate
 	cd $ROOT_DIR
 done
